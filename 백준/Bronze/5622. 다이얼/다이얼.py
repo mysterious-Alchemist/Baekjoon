ee7b251571b9099a0dc8dd1ad47dc7b314f73dee
mysterious-Alchemist <111052697+mysterious-Alchemist@users.noreply.github.com>
2022-12-31 15:57:17 +0900

    [Bronze II] Title: 다이얼, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -1,19 +1,13 @@
-lst = list(input())
-l1 = ["A", "B", "C"]
-l2 = ["D", "E", "F"]
-l3 = ["G", "H", "I"]
-l4 = ["J", "K", "L"]
-l5 = ["M", "N", "O"]
-l6 = ["P", "Q", "R", "S"]
-l7 = ["T", "U", "V"]
-l8 = ["W", "X", "Y", "Z"]
-la = [l1, l2, l3, l4, l5, l6, l7, l8]
-c = 0
-lc = []
-for i in lst:
-    for j, k in enumerate(la):
-        if(i in la[j]):
-            c = j + 3
-            lc.append(c)
-print(sum(lc))
+A, B, C = map(int, [3, 3, 3])
+D, E, F = map(int, [4, 4, 4])
+G, H, I = map(int, [5, 5, 5])
+J, K, L = map(int, [6, 6, 6])
+M, N, O = map(int, [7, 7, 7])
+P, Q, R, S = map(int, [8, 8, 8, 8])
+T, U, V = map(int, [9, 9, 9])
+W, X, Y, Z = map(int, [10, 10, 10, 10])
 
+
+n = input()
+ans = list(map(eval, n))
+print(sum(ans))
