2969a3216ea1260ffc202b31dfddffc18f0f6a73
mysterious-Alchemist <111052697+mysterious-Alchemist@users.noreply.github.com>
2023-01-22 18:38:24 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 40 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -0,0 +1,15 @@
+a,b,c,d,e,f = map(int, input().split())
+
+aa = 1 - a
+print(aa, end = " ")
+ab = 1 - b
+print(ab, end = " ")
+ac = 2 - c
+print(ac, end = " ")
+ad = 2 - d
+print(ad, end = " ")
+ae = 2 - e
+print(ae, end = " ")
+af = 8 - f
+print(af)
+
