c22b2c21a9e31800d079d97a0803ddd3e19b28a7
mysterious-Alchemist <111052697+mysterious-Alchemist@users.noreply.github.com>
2023-01-23 00:28:35 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 36824 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -2,12 +2,8 @@ from itertools import combinations
 
 n, m = map(int, input().split())
 lst = list(map(int, input().split()))
-
-lstc = list(combinations(lst, 3))
-for i in range(len(lstc)):
-  lstc[i] = sum(lstc[i])
-
-def under_m(lst):
-  return m>=lst
-lsta = list(filter(under_m, lstc))
-print(max(lsta))
+lst2 = []
+for i in combinations(lst, 3):
+  if m>=sum(i):
+    lst2.append(sum(i))
+print(max(lst2))
