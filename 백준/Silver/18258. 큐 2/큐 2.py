c379b8beac8a3b59b412c7836e910407e36a1966
mysterious-Alchemist <111052697+mysterious-Alchemist@users.noreply.github.com>
2023-02-02 21:43:52 +0900

    [Silver IV] Title: 큐 2, Time: 1616 ms, Memory: 175208 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+queue = deque()
+
+n = int(input().strip())
+for i in range(n):
+    cmd = input().strip().split()
+    if cmd[0] == 'push':
+        queue.append(cmd[1])
+    elif cmd[0] == 'pop':
+        if queue:
+            print(queue.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(queue))
+    elif cmd[0] == 'empty':
+        if queue:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'front':
+        if queue:
+            print(queue[0])
+        else:
+            print(-1)
+    elif cmd[0] == 'back':
+        if queue:
+            print(queue[-1])
+        else:
+            print(-1)
