3c688de1da08af638b5610393fa45b33e46527d6
mysterious-Alchemist <111052697+mysterious-Alchemist@users.noreply.github.com>
2023-02-13 03:09:13 +0900

    [Bronze I] Title: 더하기 사이클, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -4,34 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a;
-		a = sc.nextInt();
-		boolean chk = true;
-		if (a==0) {
-			chk = true;
-//			System.out.println(1);
-		}
-		else if (10 > a) {
-			while (10 >= a) {
-				a = a * 10 + ((2 * a) % 10);
-//					System.out.println(a);
-			}
-		}
-
-		
-		int b;
 		int c = 0;
-		b = a;
-		while (chk) {
-			int tmp1 = a % 10;
-			int tmp2 = a / 10;
-			int tmp3 = (tmp1 + tmp2)%10;
-			a = tmp1 * 10 + tmp3;
-			c = c + 1;
-//			System.out.println(a + " " + b + " " + c);
-			if (a == b) {
-				chk = false;
-				System.out.println(c);
-			}
+		a = sc.nextInt();
+		int n1 = a % 10;
+		int n2 = (a / 10) + (a % 10);
+		int n3 = (10 * n1) + (n2 % 10);
+		c = c + 1;
+		while (a != n3) {
+			n1 = n3 % 10;
+			n2 = (n3 / 10) + (n3 % 10);
+			n3 = (10 * n1) + (n2 % 10);
+			c += 1;
 		}
+		System.out.println(c);
 	}
 }
