9d241cbc7a1b0fbecebbd01c727ac0048b517436
mysterious-Alchemist <111052697+mysterious-Alchemist@users.noreply.github.com>
2023-02-24 01:18:06 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.py
@@ -1,7 +1,3 @@
 #백준 25314 코딩은 체육과목 입니다
-n = (int(input())-4)//4
-if n!=0:
-    print(n*"long ", end = "")
-    print("long int")
-else:
-    print("long int")
+print(((int(input()))//4)*"long "+"int")
+
