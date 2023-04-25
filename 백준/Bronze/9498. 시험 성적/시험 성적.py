7154437a8c31301956a5d94acea2b96dbdc6d2a1
mysterious-Alchemist <111052697+mysterious-Alchemist@users.noreply.github.com>
2023-04-25 23:40:30 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,15 @@
+while(True):
+  a = int(input())
+  if 101>a>=0:
+    break
+
+if 101>a>89:
+  print("A")
+elif a>79:
+  print("B")
+elif a>69:
+  print("C")
+elif a>59:
+  print("D")
+else:
+  print("F")
